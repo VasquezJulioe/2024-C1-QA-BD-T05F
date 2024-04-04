459561a59689cce7cbed2168fd5016ab9e052383
JohanCifuentes03 <johancfuentes03@gmail.com>
2024-04-04 14:01:22 -0500

    Se modificaron las vistas con respecto a las nuevas relaciones implementadas

--- Vistas barbería.sql
+++ Vistas barbería.sql
@@ -3,58 +3,74 @@
 -- cuáles empleados son los que más trabajan 
 
 CREATE VIEW ServiciosPorEmpleados AS
-SELECT e.idEmpleado, f.idCliente, COUNT(sa.idServicio) AS 'Servicios totales'
-FROM empleado e
-INNER JOIN factura f ON e.idEmpleado = f.idEmpleado
-INNER JOIN servicioatencion sa ON f.idElemento = sa.idAtencion
-GROUP BY e.idEmpleado, f.idCliente
-ORDER BY COUNT(sa.idServicio) DESC;
+SELECT 
+	E.idEmpleado
+    ,P.Nombre
+    ,COUNT(E.IdEmpleado) AS ServiciosRealizados
+FROM empleado E 
+INNER JOIN Persona P ON (E.IdEmpleado = P.cedula)
+INNER JOIN Factura F ON E.idEmpleado = F.idEmpleado
+INNER JOIN facturaservicioatencion FSA ON F.IdFactura = FSA.IdFactura
+INNER JOIN servicioatencion SA ON FSA.IdAtencion = SA.IdAtencion
+INNER JOIN servicio S ON (s.idServicio = SA.idServicio)
+GROUP BY E.idEmpleado ORDER BY 3 DESC;
 
--- La siguiente vista muestra los productos más consumidos en los servicios y la
--- cantidad de servicios en los que aparece
+
+-- La siguiente vista muestra la cantidad de productos consumidos por tipo de servicio, esto 
+-- nos muestra que servicios usan que componentes y que tanto, util para administrar correctamente 
+-- el inventario
 
 CREATE VIEW ProductoMasConsumido AS
-SELECT p.Nombre, COUNT(pc.idProducto) AS 'Cantidad servicios', SUM(pc.Cantidad) AS 'Consumo total'
-FROM Producto p
-JOIN ProductoConsumido pc ON p.idProducto = pc.idProducto
-GROUP BY p.Nombre
-ORDER BY SUM(pc.Cantidad) DESC;
+SELECT 
+	S.nombre AS NombreServicio
+    ,p.Nombre AS NombreProducto
+    ,SUM(PC.cantidad) AS CantidadUsada
+FROM Producto P
+INNER JOIN ProductoConsumido PC ON P.idProducto = PC.idProducto
+INNER JOIN ServicioAtencion SA ON SA.IdAtencion = PC.IdAtencion
+INNER JOIN Servicio S ON S.IdServicio = SA.IdServicio
+GROUP BY S.Nombre, P.Nombre;
 
 
 -- La siguiente vista muestra la cantidad de productos vendidos por empleado
 -- ordenados de mayor a menor 
 
 CREATE VIEW VentasPorEmpleado AS
-SELECT p.Cedula, p.Nombre AS "Nombre empleado", SUM(pc.Cantidad) AS "Productos vendidos"
-FROM persona p
-JOIN empleado e ON p.Cedula = e.idEmpleado
-JOIN factura f ON e.idEmpleado = f.idEmpleado
-JOIN productocompra pc ON f.idElemento = pc.idCompra
+SELECT 
+	p.Cedula
+    ,p.Nombre AS "Nombre empleado"
+    ,SUM(pc.Cantidad) AS "Productos vendidos"
+FROM Persona P
+INNER JOIN empleado E ON P.Cedula = E.idEmpleado
+INNER JOIN factura F ON E.idEmpleado = F.idEmpleado
+INNER JOIN facturaproductocompra FPC ON F.idFactura = FPC.IdFactura
+INNER JOIN productocompra PC ON PC.idCompra = FPC.IdCompraProducto
 GROUP BY p.Cedula, p.Nombre
 ORDER BY SUM(pc.Cantidad) DESC;
 
-
+SELECT * FROM ProductoMasConsumido
 /*
 	Esta vista se creo con la intencion de mostrar los productos comprados por un cliente, el precio, el producto y la cantidad total
     tiene la intencion de simular una factura de simplemente los productos comprados
 */
 CREATE VIEW factura_productos_comprados AS 
 SELECT 
-    F.idFactura,
-    PC.idCompra,
-    PCli.nombre     AS nombreCliente,
-    PE.nombre       AS nombreEmpleado,
-    PCli.Correo     AS correoCliente,
-    PCli.Direccion  AS direccionCliente,
-    PR.nombre 		AS nombreProducto,
-    PR.Precio 		AS precioProducto,
-    PC.Cantidad 	AS cantidadComprada,
-    PC.Total 		AS valorTotal
-FROM factura AS F
-INNER JOIN productocompra AS PC ON F.idElemento = PC.idCompra
+	F.idFactura
+    ,PCli.nombre AS "Nombre Cliente"
+    ,PE.nombre AS "Nombre Empleado"
+    ,HS.Fecha
+    ,P.Nombre AS "Nombre Producto"
+    ,PC.Cantidad AS "Cantidad Producto Comprada"
+    ,P.Precio AS "Precio Por unidad"
+    ,PC.Cantidad * P.Precio AS "Total Compra"
+FROM Factura F 
+INNER JOIN historialservicio HS ON HS.idFactura = F.idFactura
+INNER JOIN facturaproductocompra FPC ON F.idFactura = FPC.idFactura
 INNER JOIN Persona AS PE ON PE.cedula = F.idEmpleado
 INNER JOIN Persona AS PCli ON PCli.cedula = F.idCliente
-INNER JOIN Producto AS PR ON PC.idProducto = PR.idProducto;
+INNER JOIN productocompra PC ON PC.idCompra = FPC.IdCompraProducto
+INNER JOIN producto P ON P.idProducto = PC.idProducto
+ORDER BY 1;
 
 
 /*
@@ -63,68 +79,40 @@ INNER JOIN Producto AS PR ON PC.idProducto = PR.idProducto;
 */
 CREATE VIEW factura_servicios_consumidos 
 AS 
-SELECT     
-	F.idFactura,
-    SA.idAtencion,
-    PCli.nombre     AS nombreCliente,
-    PE.nombre       AS nombreEmpleado,
-    PCli.Correo     AS correoCliente,
-    PCli.Direccion  AS direccionCliente,
-    S.nombre 		AS nombreServicio,
-    S.Precio 		AS precioServicio,
-    SA.Cantidad 	AS cantidadConsumida,
-    SA.Total 		AS valorTotal
-FROM factura AS F
-INNER JOIN servicioatencion AS SA ON F.idElemento = SA.idAtencion
+SELECT 
+	F.idFactura
+    ,PCli.nombre AS "Nombre Cliente"
+    ,PE.nombre AS "Nombre Empleado"
+    ,HS.Fecha
+    ,S.Nombre AS "Nombre Servicio"
+    ,SA.Cantidad AS "Cantidad Servicio Adquirido"
+    ,S.Precio AS "Precio Servicio"
+    ,SA.Cantidad * S.precio AS "Total Servicio Adquirido"
+FROM Factura F 
+INNER JOIN historialservicio HS ON HS.idFactura = F.idFactura
 INNER JOIN Persona AS PE ON PE.cedula = F.idEmpleado
 INNER JOIN Persona AS PCli ON PCli.cedula = F.idCliente
-INNER JOIN servicio AS S ON S.idServicio = SA.idServicion;
+INNER JOIN facturaservicioatencion FSA ON F.idFactura = FSA.IdFactura
+INNER JOIN servicioatencion SA ON SA.IdAtencion = FSA.IdAtencion
+INNER JOIN servicio S ON S.idServicio = SA.idServicio
+ORDER BY 1;
 
 
 
 /*
-	Esta vista tiene la finalidad de simular una factura total, incluyendo los servicios adquiridos y los productos comprados, juntandolo todo
-    en una consulta, resulta factible cuando un cliente consumio varios servicios 
-    y aparte compro varios productos
+	Esta vista tiene la finalidad de simular el precio total de los productos y servicios adquiridos por cada cliente
+    Trabaja en conjunto con las 2 anteriores vistas 
 */
 CREATE VIEW `factura_servicios_productos_comprados_consumidos` 
 AS  
 SELECT 
-    F.idFactura,
-    PC.idCompra AS idOperacion,
-    PCli.nombre AS nombreCliente,
-    PE.nombre AS nombreEmpleado,
-    PCli.Correo AS correoCliente,
-    PCli.Direccion AS direccionCliente,
-    PR.nombre AS nombreProducto,
-    PR.Precio AS precioProducto,
-    PC.Cantidad AS cantidadComprada,
-    PC.Total AS valorTotal
-FROM factura AS F
-INNER JOIN productocompra AS PC ON F.idElemento = PC.idCompra
-INNER JOIN Persona AS PE ON PE.cedula = F.idEmpleado
-INNER JOIN Persona AS PCli ON PCli.cedula = F.idCliente
-INNER JOIN Producto AS PR ON PC.idProducto = PR.idProducto
-
-UNION ALL
-
-SELECT     
-    F.idFactura,
-    SA.idAtencion AS idOperacion,
-    PCli.nombre AS nombreCliente,
-    PE.nombre AS nombreEmpleado,
-    PCli.Correo AS correoCliente,
-    PCli.Direccion AS direccionCliente,
-    -- SA.idAtencion AS idAtencion,
-    S.nombre AS nombreProducto,
-    S.Precio AS precioProducto,
-    SA.Cantidad AS cantidadConsumida,
-    SA.Total AS valorTotal
-FROM factura AS F
-INNER JOIN servicioatencion AS SA ON F.idElemento = SA.idAtencion
-INNER JOIN Persona AS PE ON PE.cedula = F.idEmpleado
-INNER JOIN Persona AS PCli ON PCli.cedula = F.idCliente
-INNER JOIN servicio AS S ON S.idServicio = SA.idServicion;
+	FPC.IdFactura
+    ,FPC.`Nombre Cliente`
+    ,FPC.`Nombre Empleado`
+    ,SUM(FPC.`Total Compra` + FSC.`Total Servicio Adquirido`) AS "Monto Total"
+FROM factura_servicios_consumidos FSC 
+INNER JOIN factura_productos_comprados FPC ON FSC.IdFactura = FPC.IdFactura
+GROUP BY FPC.IdFactura
 
 
 
